c8c13c84e1309cd3924f082956e76922b1e82e16
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-03-04 12:35:02 +0530

    Assignment-1 4th question is completed

---
+++ java/assignment_1/Students.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Students {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of students: ");
+        int size = sc.nextInt();
+        
+        List<String> names = new ArrayList<String>();
+
+        for (int i = 0; i < size; i++){
+            System.out.print("Enter the name: ");
+            String name = sc.next();
+
+            names.add(name);
+        }
+
+        System.out.println("\nNames before sorting\n" + names);
+        names.sort(null);
+        System.out.println("\nNames after sorting\n" + names);
+    }
+}
