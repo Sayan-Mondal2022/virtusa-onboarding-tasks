3e4211f9b6f416dedef7029be614cfeef232bec5
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-03-04 13:04:42 +0530

    assignment-1 Q5 & Q6 is completed

---
+++ java/assignment_1/CommonElements.java
@@ -0,0 +1,31 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class CommonElements {
+    public static void main(String[] args) {
+        // Initializing two sets
+        Set<Integer> set1 = new HashSet<Integer>();
+        Set<Integer> set2 = new HashSet<Integer>();
+
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+        set2.add(8);
+
+        System.out.print("Elements in set1: " + set1);
+        System.out.print("\nElements in set2: " + set2);
+
+        System.out.print("\nCommon Elements are: ");
+        for (int n: set1) {
+            if (set2.contains(n))
+                System.out.print(n + " ");
+        }
+    }
+}
---
+++ java/assignment_1/ReverseLinkedList.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class ReverseLinkedList {
+
+    public static LinkedList<Integer> reverseList(LinkedList<Integer> list){
+        return list.reversed();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+
+        System.out.print("Linked list before reversing: ");
+        System.out.println(list);
+        
+        LinkedList<Integer> newList = reverseList(list);
+        System.out.print("\nLinked list before reversing: ");
+        System.out.println(newList);
+    }   
+}
