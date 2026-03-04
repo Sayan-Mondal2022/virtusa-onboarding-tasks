7a69e341d928570e706e268b3cee11642ab09d25
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-03-04 12:02:41 +0530

    Assignment-1 3rd question is completed

---
+++ java/assignment_1/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+    public String id, name;
+    public static String companyName;
+
+    static {
+        companyName = "ABC inc";
+    }
+
+    public static void changeCompanyName(String newCompanyName) {
+        System.out.println("Company name has been changed from '" + companyName + "' to '" + newCompanyName + "'\n");
+        companyName = newCompanyName;
+    }
+
+    public void displayEmployeeDetails() {
+        System.out.printf("Employee Id: %s\nEMployee name: %s\nCompany Name: %s\n\n", this.id, this.name, companyName);
+    }
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+
+        e1.id = "EP045";
+        e1.name = "Sayan";
+
+        e1.displayEmployeeDetails();
+
+        Employee.changeCompanyName("XYZ inc");
+
+        e1.displayEmployeeDetails();
+    }
+}
