f6a8d8c939ba01d6c9623a766ebeb35b98a7c043
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-03-13 11:56:41 +0530

    Question - 3 is completed

---
+++ java/assignment_2/numbers/EvenOrOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0)
+            System.out.println("The entered number " + num + " is EVEN");
+        else
+            System.out.println("The entered number " + num + " is ODD");
+    }
+}
