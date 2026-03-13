2776a6b32ce115b9cc10887df5433900fb24c480
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-03-13 12:12:19 +0530

    Question - 4 is completed

--- java/assignment_2/numbers/SumOfDigits.java
+++ java/assignment_2/numbers/SumOfDigits.java
@@ -1,25 +1,40 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SumOfDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
 
-        System.out.print("Enter the number of elements in array: ");
-        int size = sc.nextInt();
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        int total = 0;
+        int temp = num;
 
-        System.out.println();
-        for(int i = 0; i < size; i++){
-            System.out.print("Enter the number: ");
-            int n = sc.nextInt();
+        while (temp > 0){
+            total += temp % 10;
+            temp = temp / 10;
+        }
 
-            arr.add(n);
-        } 
+        System.out.println("The sum of digits for a given number '" + num + "' is: " + total);
 
-        System.out.print("\nThe array elements are: " + arr);
-        int total = arr.stream().mapToInt(Integer::intValue).sum();
 
-        System.out.println("\n\nThe sum is: " + total);
+        // This is for sum of array elements:
+        // ArrayList<Integer> arr = new ArrayList<>();
+
+        // System.out.print("Enter the number of elements in array: ");
+        // int size = sc.nextInt();
+
+        // System.out.println();
+        // for(int i = 0; i < size; i++){
+        //     System.out.print("Enter the number: ");
+        //     int n = sc.nextInt();
+
+        //     arr.add(n);
+        // } 
+
+        // System.out.print("\nThe array elements are: " + arr);
+        // int total = arr.stream().mapToInt(Integer::intValue).sum();
+
+        // System.out.println("\n\nThe sum is: " + total);
     }
 }
